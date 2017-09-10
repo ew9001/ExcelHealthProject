b9e24092e6611b055535ea56bf543969ecf0c89f
Earl <earlquality@gmail.com>
2017-09-10 04:54:24 -0400

    annotations added

--- src/test/java/com/excelhealth/mytest_cases/PageObjectModel.java
+++ src/test/java/com/excelhealth/mytest_cases/PageObjectModel.java
@@ -11,6 +11,7 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 
 import org.openqa.selenium.WebElement;
+import org.testng.SkipException;
 import org.testng.annotations.*;
 
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -18,26 +19,29 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import com.excelhealth.page_objects.LoginPageObject;
 import com.excelhealth.page_objects.MyPortalPageObject;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.apache.bcel.generic.Select;
 
 
 
 // Import package utility.*
 
 
-public class PageObjectModel  {
+public class PageObjectModel {
     private WebDriver driver;
     private String baseUrl;
 
     @BeforeTest
     public void setUp() throws Exception {
 
-        System.setProperty("webdriver.chrome.driver", "//Users/earl.willis/Downloads/chromedriver2");
-        System.setProperty("webdriver.gecko.driver", "//Users/earl.willis/Downloads/geckodriver");
+           System.setProperty("webdriver.chrome.driver", "//Users/earl.willis/Downloads/chromedriver2");
+           System.setProperty("webdriver.gecko.driver", "//Users/earl.willis/Downloads/geckodriver");
 
 
         ChromeOptions options = new ChromeOptions();
-        // options.addArguments("headless");
-        // options.addArguments("window-size=1440x1280");
+       //  options.addArguments("headless");
+         options.addArguments("window-size=1440x1280");
 
         driver = new ChromeDriver(options);
 
@@ -50,6 +54,9 @@ public class PageObjectModel  {
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
+
+
+
     @Test( priority = 1 )
     public void testLogin() {
 
@@ -57,95 +64,69 @@ public class PageObjectModel  {
         LoginPageObject.filldestinationTextBox(driver, "upwork");
         LoginPageObject.clickOnSearchButton(driver);
 
-        if(driver.getPageSource().contains("My Portal")){
-            System.out.println("User logged in successfully");
+        Assert.assertTrue(MyPortalPageObject.myPortalObject(driver).isDisplayed());
+
+        // Assert.assertEquals("Wrong error message shown", "Please Enter Valid Email", driver.findElement(By.xpath("Your path of element")).getText());
+
+//        if(driver.getPageSource().contains("My Portal")){
+//            System.out.println("User logged in successfully");
+//
+//        }else{
+//            System.out.println("User failed logging im");
+//            throw new SkipException("Skipping - This is not ready for testing ");
+//        }
+//    }
+    }
+
+    @Test( priority = 2 )
+    public void testAdd_task() throws Exception{
+
+        MyPortalPageObject.clickOnAddTaskButton(driver);
+        MyPortalPageObject.waitforstartDateTextBox(driver);
+        MyPortalPageObject.fillstartDateTextBox(driver, "2017-09-25");
+        MyPortalPageObject.fillduetDateTextBox(driver, "2017-12-25");
+        MyPortalPageObject.filldescriptionTextBox(driver, "Meet with inspectors before deadline");
+        MyPortalPageObject.fillnotesTextBox(driver,"Note: Inspection starts in 2 weeks");
+        MyPortalPageObject.clickOnSubmitButton(driver);
+        Thread.sleep(3000);
+
+        // going to have to fix this as it won't wait to go to next step w/out sleep command
+
+        if(driver.getPageSource().contains("Meet with inspe...")){
+            System.out.println("Task added successfully");
 
         }else{
-            System.out.println("User failed logging im");
+            System.out.println("User did not ass task");
+            throw new SkipException("Something bad happened ");
         }
     }
 
 
-    @Test( priority = 2 )
-    public void testAddtask() throws Exception{
-        driver.findElement(By.tagName("i")).click();
-        Thread.sleep(2000);
-
-        MyPortalPageObject.fillstartDateTextBox(driver, "2017-09-25");
-        MyPortalPageObject.fillduedateTextBox(driver, "2017-12-25");
-
-     //  driver.findElement(By.xpath("//table[@id='reportmenutable2']/tbody/tr/td/span/i")).click();
-      //  driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-
-
-     //   driver.findElement(By.id("created")).clear();
-     //   driver.findElement(By.id("created")).sendKeys("2017-09-25");
-
-     //   driver.findElement(By.xpath("//*[@id='duedate']")).sendKeys("2018-08-22");
-
-        //   driver.findElement(By.id("created")).sendKeys(Keys.TAB);
-        //   driver.findElement(By.id("duedate")).sendKeys(Keys.ENTER);
-
-
-        driver.findElement(By.id("duedate")).clear();
-
-        //   driver.findElement(By.name("duedate")).sendKeys("2017-08-30");
-        //  driver.findElement(By.id("duedate")).click();
-        //  driver.findElement(By.id("duedate")).sendKeys("2018-06-15");
-
-        //   WebElement objPh = driver.findElement(By.id("duedate"));
-        //   objPh.click();
-        //    Thread.sleep(1000);
-
-        WebElement wb = driver.findElement(By.name("duedate"));
-        JavascriptExecutor jse = (JavascriptExecutor)driver;
-        //   jse.executeScript("arguments[0].value='(222)222-2222';", wb);
-        jse.executeScript("document.getElementById('duedate').value='2018-06-15';");
-
-
-
-        driver.findElement(By.id("description")).clear();
-        driver.findElement(By.id("description")).sendKeys("First Task");
-        // new Select(driver.findElement(By.id("status"))).selectByVisibleText("On Hold");
-        // new Select(driver.findElement(By.id("priority"))).selectByVisibleText("Low");
-        driver.findElement(By.id("notes")).clear();
-        driver.findElement(By.id("notes")).sendKeys("Test Notes.");
-        driver.findElement(By.id("save")).click();
-        driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-
-
-
-
-
-// driver.findElement(By.xpath("(//button[@type='button'])[5]")).click();
-//driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-
-    }
-
-
-
-
-
 
     @Test( priority = 3 )
+    public void testDelete_task() throws Exception{
+
+        MyPortalPageObject.clickOnDescription(driver);
+        MyPortalPageObject.waitforEditTaskandclick(driver);
+        MyPortalPageObject.waitforstartDateTextBox(driver);
+        MyPortalPageObject.clickOnDeleteButton(driver);
+        MyPortalPageObject.waitforPortalPage(driver);
+
+        Assert.assertTrue(MyPortalPageObject.myPortalObject(driver).isDisplayed());
+
+
+
+        Thread.sleep(2000);
+    }
+
+    @Test( priority = 4 )
     public void testLogout() {
 
         MyPortalPageObject.clickonLogoutButton(driver);
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
+        Assert.assertTrue(LoginPageObject.originTextBox(driver).isDisplayed());
 
-      //  Assert.assertThat(LoginPageObject.homepageHeading(driver), is(equalTo("About us!"));
-     //   Assert.assertEquals("Welcome to the Excel Health:", driver.findElement(By.cssSelector("h5")));
-
-        if(driver.getPageSource().contains("Welcome to the Excel Health:")){
-            System.out.println("User logged out successfully ");
-
-        }else{
-            System.out.println("User did not make it to HomePage after logging out");
-        }
-
-            //    assertThat(element.getText(), is(equalTo("Welcome to the Excel Health:"));
-        //   MyPortalPageObject.assertHomePageTitle(driver);
     }
 
 
--- src/test/java/com/excelhealth/page_objects/MyPortalPageObject.java
+++ src/test/java/com/excelhealth/page_objects/MyPortalPageObject.java
@@ -1,9 +1,14 @@
 package com.excelhealth.page_objects;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+
+import java.util.concurrent.TimeUnit;
 
 
 public class MyPortalPageObject {
@@ -29,6 +34,7 @@ public class MyPortalPageObject {
     public static void clickonLogoutButton(WebDriver driver) {
         element = logoutButton(driver);
         element.click();
+
     }
 
     /**
@@ -53,6 +59,38 @@ public class MyPortalPageObject {
         element.click();
     }
 
+
+
+    ////////////////////////////////////////////////////
+
+
+
+
+    /**
+     * Returns MyPortal Object
+     * @param driver
+     * @return
+     */
+    public static WebElement myPortalObject(WebDriver driver) {
+        element = driver.findElement(By.id("home_myfac_filter"));
+         return element;
+
+    }
+
+    public static void waitforPortalPage(WebDriver driver) {
+
+        WebDriverWait myWaitVar = new WebDriverWait(driver,20);
+        myWaitVar.until(ExpectedConditions.presenceOfElementLocated(By.id("home_myfac_filter")));
+
+       // WebElement el = myWaitVar.until(ExpectedConditions.elementToBeClickable(myPortalObject(driver)));
+       // el.click();
+        //   el.sendKeys(origin);
+
+        //  element.sendKeys(origin);
+    }
+
+
+
     /**
      * Returns Created Date
      * @param driver
@@ -61,12 +99,32 @@ public class MyPortalPageObject {
     public static WebElement startdateTextBox(WebDriver driver) {
         element = driver.findElement(By.name("created"));
         return element;
+
     }
 
 
+    public static void waitforstartDateTextBox(WebDriver driver) {
+
+        WebDriverWait myWaitVar = new WebDriverWait(driver,20);
+        WebElement el = myWaitVar.until(ExpectedConditions.elementToBeClickable(startdateTextBox(driver)));
+        el.clear();
+     //   el.sendKeys(origin);
+
+        //  element.sendKeys(origin);
+    }
+
+
+
     public static void fillstartDateTextBox(WebDriver driver, String origin) {
+
+
         element = startdateTextBox(driver);
-        element.clear();
+
+     //   WebDriverWait myWaitVar = new WebDriverWait(driver,20);
+     //   WebElement el = myWaitVar.until(ExpectedConditions.elementToBeClickable(startdateTextBox(driver)));
+     //   el.clear();
+       // element.clear();
+
         element.sendKeys(origin);
     }
 
@@ -77,13 +135,125 @@ public class MyPortalPageObject {
      * @return
      */
     public static WebElement duedateTextBox(WebDriver driver) {
-        element = driver.findElement(By.id("duedate"));
+        element = driver.findElement(By.name("duedate"));
+        return element;
+       // element.clear();
+
+    }
+
+    public static void fillduetDateTextBox(WebDriver driver, String origin) {
+        element = duedateTextBox(driver);
+        element.clear();
+        element.sendKeys(origin);
+    }
+
+
+    /**
+     * Returns Due Date
+     * @param driver
+     * @return
+     */
+    public static WebElement descrioptionTextBox(WebDriver driver) {
+        element = driver.findElement(By.id("description"));
+
         return element;
     }
 
-    public static void fillduedateTextBox(WebDriver driver, String origin) {
-        element = startdateTextBox(driver);
+    public static void filldescriptionTextBox(WebDriver driver, String origin) {
+        element = descrioptionTextBox(driver);
         element.sendKeys(origin);
+    }
+
+
+
+
+    /**
+     * Returns Notes
+     * @param driver
+     * @return
+     */
+    public static WebElement notesTextBox(WebDriver driver) {
+        element = driver.findElement(By.id("notes"));
+        return element;
+    }
+
+    public static void fillnotesTextBox(WebDriver driver, String origin) {
+        element = notesTextBox(driver);
+        element.sendKeys(origin);
+    }
+
+
+
+    /**
+     * Click onSubmit button
+     * @param driver
+     */
+    public static void clickOnSubmitButton (WebDriver driver) {
+        element =  driver.findElement(By.id("save"));
+        element.click();
+    }
+
+
+    /**
+     * Returns EditTask Button
+     * @param driver
+     * @return
+     */
+    public static WebElement EditTaskButton(WebDriver driver) {
+        element = driver.findElement(By.cssSelector("div.ui-tooltip-content > div > div.tooltipedit > a.edittask"));
+        return element;
+    }
+
+
+
+
+
+
+
+    /**
+     * Click on Desription in list
+     * @param driver
+     */
+    public static void clickOnDescription (WebDriver driver) {
+        element =  driver.findElement(By.xpath("//*[@id='home_tasks']/tbody/tr/td[2]/span"));
+        element.click();
+
+    }
+
+
+
+
+   public static void waitforEditTaskandclick (WebDriver driver) {
+
+       WebDriverWait myWaitVar = new WebDriverWait(driver,20);
+       WebElement el = myWaitVar.until(ExpectedConditions.elementToBeClickable(EditTaskButton(driver)));
+       el.click();
+
+    }
+//
+//
+//    /**
+//     * Click on Edit Task Pop UP
+//     * @param driver
+//     */
+//    public static void clickOnEditTask (WebDriver driver) {
+//
+//
+//        element =  driver.findElement(By.cssSelector("div.ui-tooltip-content > div > div.tooltipedit > a.edittask"));
+//        element.click();
+//    }
+
+
+
+
+
+    /**
+     * Click on Delete button
+     * @param driver
+     */
+    public static void clickOnDeleteButton (WebDriver driver) {
+        element =   driver.findElement(By.id("delete"));
+        element.click();
     }
 
 
