55f9c02377ff07acece23d4f0891b8d2b224526d
Earl <earlquality@gmail.com>
2017-11-02 17:59:14 -0400

    Update Constants.java

--- src/test/java/com/excelhealth/utilities/Constants.java
+++ src/test/java/com/excelhealth/utilities/Constants.java
@@ -3,8 +3,8 @@ package com.excelhealth.utilities;
 public class Constants {
 
     public static final String URL = "http://trial.excelhealthportal.com/";
-    public static final String Username = "EarlW";
-    public static final String Password = "upwork";
+    public static final String Username = "*******";
+    public static final String Password = "******";
     public static final String File_Path = "//Users//earl.willis//Documents//workspace/WebdriverTest//src//test//java//com//excelhealth//utilities//";
     public static final String File_Name = "ExcelData.xlsx";
 
